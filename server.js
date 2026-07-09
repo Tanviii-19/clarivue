1b7821f62b4fe6ace943ea0495408a2ea097aaaf
Tanviii-19 <tanvisilora@gmail.com>
2026-07-09 09:09:00 +0530

    Add Aiven SSL support and update gitignore

--- server.js
+++ server.js
@@ -109,7 +109,10 @@ const db = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT
+  port: process.env.DB_PORT,
+  ssl: {
+    ca: fs.readFileSync('./ca.pem')
+  }
 });
 
 
